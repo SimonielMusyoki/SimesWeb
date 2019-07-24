593b62b122408aaf3610ceeabe4848f372769ad8
SimonielMusyoki <musyoki.mtk3@gmail.com>
2019-07-24 12:22:16 +0300

    Updated user urls

--- cfeBlog/urls.py
+++ cfeBlog/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 from blog.views import blog_post_create_view
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
@@ -25,6 +32,13 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("executives/", views.executives, name="executives"),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     
 ]
 
