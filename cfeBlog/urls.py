9a2b1a2a72b4f3b25d93fee90292415c45a2f50c
SimonielMusyoki <musyoki.mtk3@gmail.com>
2019-07-25 11:32:46 +0300

    New URLs

--- cfeBlog/urls.py
+++ cfeBlog/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("executives/", views.executives, name="executives"),
     path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profileUpdate, name='profile-new'),
+    # path('profile/', user_views.profileUpdate, name='profile-new'),
     path('accounts/profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
